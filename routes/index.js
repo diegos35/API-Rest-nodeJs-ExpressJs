f05c81bc8afffc340745b4c526096916b137ce32
diego caviedes <diego.caviedes@cargologik.com>
2022-11-17 00:13:16 -0500

    implementando version en API

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
+const express = require('express');
+
 const productsRouter = require('./products.router')
 const usersRouter = require('./users.router')
 
 
 
 function routerApi(app){
-app.use('/products', productsRouter)
-app.use('/products', usersRouter)
+  const router = express.Router();
+  app.use('/api/v1', router);
+  router.use('/products', productsRouter)
+  router.use('/products', usersRouter)
 }
 
 module.exports = routerApi;
