adfc8dd1bf7343d964e199680e27be69050d17c3
diego caviedes <diego.caviedes@cargologik.com>
2022-11-17 00:28:57 -0500

    Status code response

--- routes/products.router.js
+++ routes/products.router.js
@@ -32,7 +32,7 @@ router.get('/:id', (req, res)=>{
 
 router.post('/', (req, res)=>{
   const  body  = req.body;
-  res.json({
+  res.status(201).json({
     message: 'created',
     data: body
   })
