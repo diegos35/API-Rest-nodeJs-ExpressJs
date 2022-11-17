74d14cd8aee6647ca8a3efae1ee2372185b9d1d9
diego caviedes <diego.caviedes@cargologik.com>
2022-11-16 23:23:36 -0500

    GET recibiendo parametros

--- index.js
+++ index.js
@@ -12,12 +12,27 @@ app.get('/new-route', (req, res)=>{
   res.send('Hola, soy nueva ruta')
 })
 
-app.get('/productos', (req, res)=>{
+app.get('/products', (req, res)=>{
+  res.json([
+    {
+      name: 'Product 1',
+      price: 10000
+    },
+    {
+      name: 'Product 2',
+      price: 20000
+    }
+  ])
+})
+
+app.get('/products/:id', (req, res)=>{
+  const { id } = req.params; //destructuring
   res.json({
-    name: 'Diego',
+    id,
+    name: 'Product6',
     price: 10000
   })
-})
+});
 
 app.listen(port, () =>{
   console.log('listening'+ port);
