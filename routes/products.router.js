edb58f2818d0da790db0d06d99502f200f92d410
diego caviedes <diego.caviedes@cargologik.com>
2022-11-17 00:26:39 -0500

    put, patch and delete

--- routes/products.router.js
+++ routes/products.router.js
@@ -38,4 +38,25 @@ router.post('/', (req, res)=>{
   })
 })
 
+
+router.patch('/:id', (req, res) => {
+  const {id} = req.params;
+  const body = req.body;
+  res.json({
+    message: 'Update',
+    data: body,
+    id
+  });
+});
+
+//DELETE
+router.delete('/:id', (req, res) => {
+  const {id} = req.params;
+  res.json({
+    message: 'Deleted',
+    id
+  });
+});
+
+
 module.exports = router;
